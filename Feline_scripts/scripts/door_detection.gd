b5e9cce445bab2008e918eac932b03fb91b081b0
Steven-Cowley <144847999+Steven-Cowley@users.noreply.github.com>
2024-07-28 21:47:55 -0400

    Update door_detection.gd

--- Feline_scripts/scripts/door_detection.gd
+++ Feline_scripts/scripts/door_detection.gd
@@ -3,9 +3,12 @@ extends Area2D
 @export var doorID: String
 @onready var player_in_detection = false
 
+var current_scene: String = ""
+var previous_scene: String = ""
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -14,15 +17,43 @@ func _process(delta):
 		door_manager(doorID)
 	
 func door_manager(scene: String):
+	if scene == current_scene:
+		return
+	
+	previous_scene = current_scene
+	current_scene = scene
+	
 	match scene:
 		"hallway_3rd_from_player_room":
+			Global.next_player_pos = Vector2(-131, -16)
+			Global.should_set_pos = true
 			get_tree().change_scene_to_file("res://scenes/hallway_3a.tscn")
 		"player_room_from_hallway":
-			# REMEMBER TO CHANGE PLAYER POSITION AT THE DOOR
+			Global.next_player_pos = Vector2(121, -16)
+			Global.should_set_pos = true
 			get_tree().change_scene_to_file("res://scenes/player_room.tscn")
+		"to_3b":
+			Global.next_player_pos = Vector2(121, -16)
+			Global.should_set_pos = true
+			get_tree().change_scene_to_file("res://scenes/3b.tscn")
+		"3b_to_3rd_hallway":
+			Global.next_player_pos = Vector2(147, -16)
+			Global.should_set_pos = true
+			get_tree().change_scene_to_file("res://scenes/hallway_3a.tscn")
+		"to_3a":
+			Global.next_player_pos = Vector2(121, -16)
+			Global.should_set_pos = true
+			get_tree().change_scene_to_file("res://scenes/apt_3a.tscn")
+		"3a_to_3rd_hallway":
+			Global.next_player_pos = Vector2(427.5, -16)
+			Global.should_set_pos = true
+			get_tree().change_scene_to_file("res://scenes/hallway_3a.tscn")
 	
 func _on_body_entered(body):
-	player_in_detection = true
+	if body.name == "Player":
+		player_in_detection = true
 	
 func _on_body_exited(body):
-	player_in_detection = false
+	if body.name == "Player":
+		player_in_detection = false
+
